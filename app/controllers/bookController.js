5a0246c3021e3ee89c45106dfc5fe9c3add96b76
Filip Wójtowicz <filipwojtowicz@Filips-MacBook-Pro.local>
2020-03-26 19:00:57 +0100

    heroku init

--- app/controllers/bookController.js
+++ app/controllers/bookController.js
@@ -1,18 +1,18 @@
-const db = require("../models")
-const Book = db.books
-const Op = db.Sequelize.Op
+const db = require("../models");
+const Book = db.books;
+const Op = db.Sequelize.Op;
 
-exports.create = (req,res) => {
+exports.create = (req, res) => {
   if (!req.body.data.isbn) {
     res.status(400).send({
       message: "isbn is required damn it"
-    })
-    return
+    });
+    return;
   }
 
-  const book =  {
+  const book = {
     isbn: req.body.data.isbn,
-    title: req.body.data.title ,
+    title: req.body.data.title,
     author: req.body.data.author,
     publication_year: req.body.data.publication_year,
     place_of_publication: req.body.data.place_of_publication,
@@ -23,105 +23,110 @@ exports.create = (req,res) => {
     whoLend: req.body.data.whoLend,
     whoBorrowed: req.body.data.whoBorrowed,
     isRequested: req.body.data.isRequested ? req.body.data.isRequested : 0
-  }
+  };
 
   Book.create(book)
-  .then(data => {
-    res.send(data)
-  }).catch(err => {
-    res.status(500).send({
-      message:
-      err.message || "Error occured while creating the book"
+    .then(data => {
+      res.send(data);
+      console.log("SERVER HERE");
     })
-  })
-}
+    .catch(err => {
+      res.status(500).send({
+        message: err.message || "Error occured while creating the book"
+      });
+    });
+};
 
 exports.findAll = (req, res) => {
-  const title = req.query.title
+  const title = req.query.title;
   var condition = title ? { title: { [Op.iLike]: `%${title}%` } } : null;
 
-  Book.findAll({where: condition}) 
-  .then(data => {
-    res.send(data)
-  }).catch(err=> {
-    res.status(500).send({
-      message:
-      err.message || "Error occurred while retrieving tutorials"
+  Book.findAll({ where: condition })
+    .then(data => {
+      res.send(data);
     })
-  })
-}
+    .catch(err => {
+      res.status(500).send({
+        message: err.message || "Error occurred while retrieving tutorials"
+      });
+    });
+};
 
 exports.findOne = (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
 
   Book.findByPk(id)
-  .then(data => {
-    res.send(data)
-  }).catch(err => {
-    res.status(500).send({
-      message: "Error retrieving book with ID" + id
+    .then(data => {
+      res.send(data);
     })
-  })
-}
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving book with ID" + id
+      });
+    });
+};
 
 exports.update = (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
   Book.update(req.body.data, {
-    where: {id: id}
-  }) 
-  .then (num => {
-    if (num == 1) {
-      res.send({
-        message: "Book was updated successfully"
-      })
-    }
-    else {
-      res.send ({
-        message: "Cannot update book with id " + id
-      })
-    }
-  }).catch(err => {
-    res.status(500).send({
-      message: "Error updating book with id " +id
-    })
+    where: { id: id }
   })
-}
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Book was updated successfully"
+        });
+      } else {
+        res.send({
+          message: "Cannot update book with id " + id
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating book with id " + id
+      });
+    });
+};
 
 exports.delete = (req, res) => {
-  const id = req.params.id
+  const id = req.params.id;
 
   Book.destroy({
-    where: {id: id}
-  }).then(num => {
-    if(num == 1) {
-      res.send({
-        message: "Book was deleted successfully"
-      })
-    } else {
-      res.send({
-        message: "Cannot delete book with id" + id
-      })
-    }
-  }).catch(err => {
-    res.status(500).send({
-      message: "Error deleting book with id" + id
-    })
+    where: { id: id }
   })
-}
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "Book was deleted successfully"
+        });
+      } else {
+        res.send({
+          message: "Cannot delete book with id" + id
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error deleting book with id" + id
+      });
+    });
+};
 
 exports.deleteAll = (req, res) => {
   Book.destroy({
     where: {},
     truncate: false
   })
-  .then(nums => {
-    res.send({message: `{nums} Books were deleted successfully`})
-  }).catch(err => {
-    res.status(500).send({
-      message: err.message || "Error occurred while deleting all books"
+    .then(nums => {
+      res.send({ message: `{nums} Books were deleted successfully` });
     })
-  })
-}
+    .catch(err => {
+      res.status(500).send({
+        message: err.message || "Error occurred while deleting all books"
+      });
+    });
+};
 
 // exports.findAllBorrowed = (req,res) => {
 
--- server.js
+++ server.js
@@ -1,34 +1,30 @@
-const express = require("express")
-const bodyParser = require ("body-parser")
-const cors = require ("cors")
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-const app = express()
+const app = express();
 
 var corsOptions = {
   origin: "http://localhost:8080"
-}
+};
 
-app.use(cors(corsOptions))
+app.use(cors(corsOptions));
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }));
 
-const db = require("./app/models")
-
+const db = require("./app/models");
 
 db.sequelize.sync({ force: true }).then(() => {
   console.log("Drop and re-sync db.");
 });
 
 app.get("/", (req, res) => {
-  res.json({message: "Welcome"})
-})
+  res.json({ message: "Welcome in eureka.js" });
+});
 
-require("./app/routes/bookRoutes")(app)
+require("./app/routes/bookRoutes")(app);
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
-
-
-
