57c1107602b8726e3c40d51a78a6e61513881b47
Filip Wójtowicz <filipwojtowicz@Filips-MacBook-Pro.local>
2020-03-27 17:21:12 +0100

    it's a typo

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "https://eurekajs.netlify.com/"
+  origin: "https://eurekajs.netlify.com"
 };
 
 app.use(cors(corsOptions));
