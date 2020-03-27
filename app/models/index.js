bce3c7b48d797366823913b4e3ac0a647d4bc586
Filip Wójtowicz <filipwojtowicz@Filips-MacBook-Pro.local>
2020-03-27 14:47:44 +0100

    now or never

--- app/models/index.js
+++ app/models/index.js
@@ -1,22 +1,33 @@
-const dbConfig = require("../config/db.config.js")
-const Sequelize = require("sequelize")
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  operatorsAliases: 0,
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle
-  }
-})
+const dbConfig = require("../config/db.config.js");
+const Sequelize = require("sequelize");
 
-const db = {}
+if (process.env.DATABASE_URL) {
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
+    port: 5432,
+    host: "<heroku host>",
+    logging: true
+  });
+} else {
+  sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    operatorsAliases: 0,
+    pool: {
+      max: dbConfig.pool.max,
+      min: dbConfig.pool.min,
+      acquire: dbConfig.pool.acquire,
+      idle: dbConfig.pool.idle
+    }
+  });
+}
 
-db.Sequelize = Sequelize
-db.sequelize = sequelize
+const db = {};
 
-db.books = require("./bookModel.js")(sequelize, Sequelize)
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
 
-module.exports = db
+db.books = require("./bookModel.js")(sequelize, Sequelize);
+
+module.exports = db;
