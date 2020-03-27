ad29657668ed5198eaf23f4b57f055d27e18bf7b
Filip Wójtowicz <filipwojtowicz@Filips-MacBook-Pro.local>
2020-03-27 17:19:10 +0100

    god

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: ["http://localhost:8080", "https://eurekajs.netlify.com/"]
+  origin: "https://eurekajs.netlify.com/"
 };
 
 app.use(cors(corsOptions));
