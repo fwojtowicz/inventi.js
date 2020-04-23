d8ecd5279ef028b4ebc998145761658a428989d1
Filip <fil.wojtowicz@gmail.com>
2020-04-23 13:55:10 +0200

    WIP, google login CORS to fix

--- server.js
+++ server.js
@@ -10,6 +10,28 @@ const passport = require("passport")
 require("dotenv").config({ path: ".env" })
 
 const app = express()
+
+// app.use("*", (req, res, next) => {
+//   if (req.method == "OPTIONS") {
+//     res.status(200)
+//     res.send()
+//   } else {
+//     next()
+//   }
+// })
+
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "http://localhost:8080/login")
+//   res.header(
+//     "Access-Control-Allow-Methods",
+//     "GET,POST,OPTIONS,PUT,PATCH,DELETE"
+//   )
+//   res.header("Access-Control-Allow-Headers", "X-Requested-With,Content-Type")
+//   res.header("Access-Control-Allow-Credentials", true)
+
+//   next()
+// })
+
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
